8a8937f23dbbda35febae0fec7eeb9c7bdd67c23
infiniteCable2 <steffen.dahansi.werner@googlemail.com>
2025-09-26 21:27:55 +0200

    Update controlsd.py

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -66,6 +66,7 @@ class Controls(ControlsExt, ModelStateBase):
     self.enable_smooth_steer = self.params.get_bool("EnableSmoothSteer")
     self.smooth_steer = PT2Filter(46.0, 1.0, DT_CTRL)
     self.force_rhd_for_bsm = self.params.get_bool("ForceRHDForBSM")
+    self.force_steer_angle_offset = float(self.params.get("AngleOffsetDegree"))
 
     self.pose_calibrator = PoseCalibrator()
     self.calibrated_pose: Pose | None = None
@@ -97,6 +98,7 @@ class Controls(ControlsExt, ModelStateBase):
       self.enable_speed_limit_control = self.params.get_bool("EnableSpeedLimitControl")
       self.enable_speed_limit_predicative = self.params.get_bool("EnableSpeedLimitPredicative")
       self.force_rhd_for_bsm = self.params.get_bool("ForceRHDForBSM")
+      self.force_steer_angle_offset = float(self.params.get("AngleOffsetDegree"))
   
   def state_control(self):
     CS = self.sm['carState']
@@ -192,6 +194,7 @@ class Controls(ControlsExt, ModelStateBase):
     CC.curvatureControllerActive = self.enable_curvature_controller # for car controller curvature correction activation
     CC.steerLimited = self.steer_limited_by_safety
     CC.forceRHDForBSM = self.force_rhd_for_bsm
+    CC.forceSteerAngleOffset = self.force_steer_angle_offset
 
     # Orientation and angle rates can be useful for carcontroller
     # Only calibrated (car) frame is relevant for the carcontroller
